f92403e43b484129f57b1a131303284d616421ae
stella <snjeri6@gmail.com>
2019-04-30 19:32:06 +0300

    Assesment test

---
+++ Python Class/Example 5/Shapes.py
@@ -0,0 +1,76 @@
+class Circle:
+	
+	def __init__(self,radius):
+		self.radius=radius
+
+	def Area(self):
+ 		self.A=(27/7*radius*radius);
+ 		Print(A)
+		
+	def circumfrence(self):
+ 		self.C=2(27/7*radius);
+ 		print(C)
+ class square:
+ 	def __init__(self,a):
+ 		self.a
+
+ 	def Area(self):
+ 		A=self.a*a
+ 		print(A)
+
+ 	def perimeter(self):
+ 		P=4*self.a
+ 		print(P)
+
+ class rectangle:
+ 	def __init__(self,w,l):
+ 	 	self.w+l
+
+ 	def area:
+ 		A=self.W+L
+ 		print(A)
+
+ 	def perimeter:
+ 		P=self.2(L+W);
+ 		print(P)
+
+ class Sphere:
+ 	def __init__(self,r):
+ 		self.r*r
+
+ 	def Surface_Area:
+ 		A=4*self.(27/7)*2
+ 		print(A)
+
+
+
+
+ 		
+
+
+
+
+
+
+
+
+
+ 		
+ 		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
