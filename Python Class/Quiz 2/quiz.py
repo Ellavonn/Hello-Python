f3f8e63a8beb1df8251c4da299ad36b6377380ed
stella <snjeri6@gmail.com>
2019-05-07 19:18:35 +0300

    quiz 2

---
+++ Python Class/Quiz 2/quiz.py
@@ -0,0 +1,93 @@
+def tens(s):
+	s= range(0,10)
+	for x in y;
+	s=
+	s=0*10
+	return n
+
+
+def sorted:
+	a=[9,1,4,7,3]
+	b=[5,8,2,6,0]
+	c=a+b
+	c.sort()
+
+
+def range_sum(i,n)
+	x=[]
+	for y in range(1,n+1):
+		x.append(y)
+		b=sum(x)
+	return(b)
+
+
+def largest(i):
+	n=[]
+	for x in lists:
+		if x%i==0;
+		n.append(x)
+		print(n)
+		print(max(n))
+
+def smallest(i):
+	a=[]
+	for x in lists:
+		if x-i==0;
+		n.append(x)
+		print(n)
+		print(min(n))
+
+
+def students(i)
+student=[{'balance': 1000, 'name': 'Irene'}, {'balance': 2000, 'name': 'Pauline'}, {'balance': 3000, 'name': 'Naima'}, {'balance': 1000, 'name': 'Rose'}]
+
+student1={"name":"Irene","balance":1000}
+student2={"nanme":"Pauline","balance":2000}
+student3={"name":"Naima","balance":3000}
+student4={"name":"Rose","balance":1000}
+for s in students:
+	print="hello{}, your balance is Ksh{}".format(name,amount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
